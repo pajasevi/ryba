59275bc8181d0fc2109d803298d070e6241d734f
Pavel Ševčík <pajaseviwow@gmail.com>
2015-01-16 16:38:11 +0100

    vsdvdfsb

--- tooltip.js
+++ tooltip.js
@@ -5,10 +5,18 @@
 $(function() {
   $('.tooltip').on('mouseover', function() {
     var tooltipContent = $(this).attr('data-tooltip');
-    var elemTop = $(this).offset.top;
+    var elemTop = $(this).offset.top - 20;
     var elemCenter = $(this).offset.left + ( $(this).innerWidth() / 2 );
 
     $('body').append('<div id="tooltipContent">' + tooltipContent + '</div>');
+
+    var tooltipLeft = elemCenter - $('tooltipContent').outerWidth();
+
+    $('#tooltipContent').css({
+      position: "absolute",
+      top: elemTop,
+      left: tooltipLeft
+    });
   });
 
   $('.tooltip').on('mouseout', function() {
